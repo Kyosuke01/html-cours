0bd26fa32d435a8b138ae7004e637a814b58e02e
Kyosuke <93272866+Kyosuke01@users.noreply.github.com>
2022-03-28 14:08:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+btn1.onclick = () => document.body.style.backgroundColor = "blue";
+btn2.onclick = () => document.body.style.backgroundColor = "purple";
+btn3.onclick = () => document.body.style.backgroundColor = "green";
+btn4.onclick = () => document.body.style.backgroundColor = "yellow";
+btn5.onclick = () => document.body.style.backgroundColor = "orange";
+btn6.onclick = () => document.body.style.backgroundColor = "white";
+btn7.onclick = () => document.body.style.backgroundColor = "#505050";
